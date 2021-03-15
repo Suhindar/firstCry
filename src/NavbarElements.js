10b241e37b7fb1ac4a1351cf48aa2ffe869449f6
Suhindar <suhiviper@gmail.com>
2021-03-15 19:09:46 +0530

    Images were added to this site

--- src/NavbarElements.js
+++ src/NavbarElements.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import {useState} from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import {FormControl} from 'react-bootstrap';
+import { Nav, Navbar,FormControl,Form} from 'react-bootstrap';
+import {Carousel,CarouselItem} from 'react-bootstrap';
+
 import {Button} from 'react-bootstrap';
 function NavbarElements() {
     return (
         <div className='App'>
             <div>
                 <Navbar>
-                        <img src="/search-icon.svg"></img>
-                        <Nav className="mr-sm-2">
-                            <img src="/"    
+                        <img src="fc_logo.png"></img>
+                        <Nav className="mr-auto">
+                        <img src="search-icon.svg"/>&nbsp;
+                        <Nav.Link href="#App">Stores & pre-schools</Nav.Link>&nbsp;
+                        <Nav.Link href="#App">Support</Nav.Link>&nbsp;
+                        <Nav.Link href="#App">Track Order</Nav.Link>&nbsp;
+                            <Nav.Link href="App">
+                                
+                            </Nav.Link>
+                            
                         </Nav> 
                 </Navbar>
+
+                <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="\de9b84a4-b8c1-4a85-b57f-5081ca2cdb78.png"
+      alt="Mammy Poko Pants"/>
+    </Carousel.Item>
+    <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="\R75e6d835fa411f156e1d3f3b86ba9e8d.jpg"
+      alt="Mammy Poko Pants"/>
+    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     );
